a35c03a3ef52924c744c7c174acad4d9bdb6e5fa
Deep Patel <deepgpatel93@gmail.com>
2018-02-05 23:58:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function theBeatlesPlay(muscians, instruments){
   }  
   return beatlesPlay 
 }
+
+function johnLennonFacts(){
+  var facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"]
+  var i 
+  while (i < facts.length){
+     moreFacts = facts.push( facts[i] +" !!! ")
+  }
+return morefacts  
+}
