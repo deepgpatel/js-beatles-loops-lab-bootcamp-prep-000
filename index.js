1315e42eb8eb56adddeb1e04d0d354f23b39668b
Deep Patel <deepgpatel93@gmail.com>
2018-02-05 23:58:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay(muscians, instruments){
+  var beatlesPlay = []
+  var i
+  for (i = 0; i < muscians.length; i++) {
+    if (instruments[i]) {
+    beatlesPlay = [+ muscians + "plays" + instruments + "."];
+    } else {
+      
+    }
+  
+  }  
+  return beatlesPlay 
+}
