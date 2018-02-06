5269bd1d9edfba76a2d4db051ed1ba82623ed4a5
Deep Patel <deepgpatel93@gmail.com>
2018-02-06 00:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 function theBeatlesPlay(muscians, instruments){
-  var beatlesPlay = []
-  var i
+  var beatlesPlay = [];
+  var i;
   for (i = 0; i < muscians.length && i < instruments.length; i++) {
-    beatlesPlay.push( muscians[i] +" plays "+ instruments[i])
+    beatlesPlay.push( muscians[i] +" plays "+ instruments[i]);
     }  
-return beatlesPlay 
+return beatlesPlay ;
 }
 
 function johnLennonFacts(facts){
-  var moreFacts = []
-  var i = 0
+  var moreFacts = [];
+  var i = 0;
   while (i < facts.length){
-     moreFacts.push( facts[i] +"!!!")
-     i++ 
+     moreFacts.push( facts[i] +"!!!");
+     i++; 
   }
-return moreFacts  
+return moreFacts;  
 }
 
-function
+function iLoveTheBeatles (n){
+  var theBeatles = [];
+  
+}
+
+
+
+
+
+
+
+
+
+
+
