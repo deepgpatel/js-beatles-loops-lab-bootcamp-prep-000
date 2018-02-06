7c65a66ae199b9314af53dfb437cc9a686a12c45
Deep Patel <deepgpatel93@gmail.com>
2018-02-06 00:04:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ return beatlesPlay
 }
 
 function johnLennonFacts(){
-  var facts = [
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"]
   var moreFacts = []
   var i 
   while (i < facts.length){
