6125d700c61fbc6dbb66189da6c48336a7e840d6
Deep Patel <deepgpatel93@gmail.com>
2018-02-06 00:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ return moreFacts;
 
 function iLoveTheBeatles (n){
   var theBeatles = [];
-  var i = 0;
+  var i = 15;
   do {
     theBeatles.push("I love the Beatles!");
     i++;
-  } while (i > n.length);
+  } while (i < n.length);
 return theBeatles;
 }
 
